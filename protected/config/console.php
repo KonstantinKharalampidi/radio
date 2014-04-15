c6b6bc955adcae5cd223b1b674544ab45444e997
gudson <gudson@outlook.com>
2014-04-16 00:04:24 +0300

    Модуль регистрации и авторизации, + конфиг

--- protected/config/console.php
+++ protected/config/console.php
@@ -11,19 +11,18 @@ return array(
 
 	// application components
 	'components'=>array(
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
+//		'db'=>array(
+//			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+//		),
 		// uncomment the following to use a MySQL database
-		/*
+
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=radio',
 			'emulatePrepare' => true,
-			'username' => 'root',
+			'username' => 'radio',
 			'password' => '',
 			'charset' => 'utf8',
 		),
-		*/
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
@@ -33,5 +32,19 @@ return array(
 				),
 			),
 		),
+        'modules'=>array(
+            'user'=>array(
+                'hash' => 'md5',
+                'sendActivationMail' => false,
+                'loginNotActiv' => false,
+                'activeAfterRegister' => true,
+                'autoLogin' => true,
+                'registrationUrl' => array('/user/registration'),
+                'recoveryUrl' => array('/user/recovery'),
+                'loginUrl' => array('/user/login'),
+                'returnUrl' => array('/user/profile'),
+                'returnLogoutUrl' => array('/user/login'),
+            ),
+        ),
 	),
 );
--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,9 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'Радио ОНПУ',
+    'sourceLanguage' => 'en',
+    'language' => 'ru',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -16,18 +18,30 @@ return array(
 	'import'=>array(
 		'application.models.*',
 		'application.components.*',
+        'application.modules.user.models.*',
+        'application.modules.user.components.*',
 	),
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'gii',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
+        'user'=>array(
+            'hash' => 'md5',
+            'sendActivationMail' => false,
+            'loginNotActiv' => false,
+            'activeAfterRegister' => true,
+            'autoLogin' => true,
+            'registrationUrl' => array('/user/registration'),
+            'recoveryUrl' => array('/user/recovery'),
+            'loginUrl' => array('/user/login'),
+            'returnUrl' => array('/user/profile'),
+            'returnLogoutUrl' => array('/user/login'),
+        ),
 	),
 
 	// application components
@@ -35,6 +49,7 @@ return array(
 		'user'=>array(
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
+            'class' => 'WebUser',
 		),
 		// uncomment the following to enable URLs in path-format
 		/*
@@ -47,19 +62,18 @@ return array(
 			),
 		),
 		*/
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
+//		'db'=>array(
+//			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+//		),
 		// uncomment the following to use a MySQL database
-		/*
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+            'tablePrefix' => 'tbl_',
+			'connectionString' => 'mysql:host=localhost;dbname=radio',
 			'emulatePrepare' => true,
-			'username' => 'root',
+			'username' => 'radio',
 			'password' => '',
 			'charset' => 'utf8',
 		),
-		*/
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
@@ -85,6 +99,6 @@ return array(
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
+		'adminEmail'=>'radio@onpu.od.ua',
 	),
 );
